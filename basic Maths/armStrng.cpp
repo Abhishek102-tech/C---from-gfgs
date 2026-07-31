cff634df11ddf8effcd382a14d71ad56f133790f
Abhishek102-tech <181866251+Abhishek102-tech@users.noreply.github.com>
2026-07-31 18:34:42 +0530

    updated armstrong number code

--- basic Maths/armStrng.cpp
+++ basic Maths/armStrng.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 void armStrong(int n){
     int sum = 0;
     int sub = n;
-    int d = 0;
+    int d = 0,dig=0;
+    int k = n;
+
+    while(k!=0){
+        k=k/10;
+        dig++;
+    }
+    
     while(sub!=0){
         d = sub%10;
-        sum = sum + (d*d*d);
+        sum = sum + pow(d,dig);
         sub = sub/10;
     }
     if(sum==n){
