19c217e72f8df39be4af7176bc1db71defff9321
Abhishek102-tech <181866251+Abhishek102-tech@users.noreply.github.com>
2026-08-07 21:43:33 +0530

    uncompleted hashMap code

---
+++ hashing/hashMap.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void constructMap(int arr[],int hashMap[][2],int n){
+    int hashmap={0};
+    for(int i=0;i<n;i++){
+        hashmap[];
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter the number of elements :";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter array elements :"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int hashMap[n][2];
+    constructMap(arr,hashMap,n);
+}
