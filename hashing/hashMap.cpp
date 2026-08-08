3251e0e49bce91833aeb18ee30eb77346de26ee4
Abhishek102-tech <181866251+Abhishek102-tech@users.noreply.github.com>
2026-08-08 15:05:51 +0530

    made my first hashmap

--- hashing/hashMap.cpp
+++ hashing/hashMap.cpp
@@ -3,9 +3,20 @@
 using namespace std;
 
 void constructMap(int arr[],int hashMap[][2],int n){
-    int hashmap={0};
+    
     for(int i=0;i<n;i++){
-        hashmap[];
+        for(int j=0;j<n;j++){
+        if(arr[i]==hashMap[j][1]){
+            hashMap[j][2]++;
+            break;
+        }
+        }
+            int j=0;
+            while(hashMap[j][1]!=0){
+                j++;
+            }
+            hashMap[j][1]=arr[i];
+            hashMap[j][2]=1; 
     }
 }
 
@@ -18,6 +29,15 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    int hashMap[n][2];
+    int hashMap[n][2]={0};
     constructMap(arr,hashMap,n);
+    int ele;
+    cout<<"Enter the number :";
+    cin>>ele;
+    int num=0;
+    while (hashMap[num][1]!=ele)
+    {
+        num++;
+    }
+    cout<<"The number of occurrences of "<<ele<<" in the array :"<<hashMap[num][2];
 }
